732ca995746d0692ccd13ae48a1bd7df4df04085
JoaoTeixeira89218 <joao07um@gmail.com>
2021-05-04 23:31:46 +0100

    auto done

--- assets/multistepform/js/msform.js
+++ assets/multistepform/js/msform.js
@@ -7,23 +7,24 @@ var animating; //flag to prevent quick multi-click glitches
 
 $(".next").click(function () {
 	var btn = $(this)[0].id;
+
 	if (btn == "orderType") TIPO = getTIPO();
-	if (TIPO == null) return
+	if (TIPO == null) return;
 
 	if (btn == "orderPrice") PRECO = getPRECO(); loadDuration();
-	if (PRECO == undefined) return
+	if (PRECO == undefined) return;
 
 	if (btn == "orderDuration") DURACAO = getDURACAO();
-	if (DURACAO == undefined) return
+	if (DURACAO == undefined) return;
 
 	if (btn == "orderClassification") CLASSIFICACAO = getCLASSIFICACAO();
-	if (CLASSIFICACAO == undefined) return
+	if (CLASSIFICACAO == undefined) return;
 
 	if (btn == "orderCategory") CATEGORIA = getCATEGORIA();
-	if (CATEGORIA == undefined) return
+	if (CATEGORIA == undefined) return;
 
 	if (btn == "orderExtra") EXTRAS = getEXTRAS();
-	if (EXTRAS == null) return
+	if (EXTRAS == null) return;
 
 	if (animating) return false;
 	animating = true;
@@ -98,6 +99,37 @@ $(".previous").click(function () {
 	});
 });
 
+//******************************************************************************************************************************************** */
+
+const recomendationOption = document.getElementById("recomendationOption");
+recomendationOption.addEventListener("click", () => {
+	OPCAO = getOPCAO();
+	if (OPCAO == null) return;
+	document.getElementById("optionSection").style.display = 'none';
+	document.getElementById("questionsSection").style.display = 'block';
+})
+
+const recomendationOptionPrevious = document.getElementById("recomendationOptionPrevious");
+recomendationOptionPrevious.addEventListener("click", () => {
+	document.getElementById("questionsSection").style.display = 'none';
+	document.getElementById("optionSection").style.display = 'flex';
+})
+
+//Select option of order  (imagens)
+$(document).on('click', '.optionArea', function () {
+	var element = $(this)[0].children[0];
+	if ($(".optionAreaSelected")[0]) {
+		if (element.classList.contains('optionAreaSelected')) {
+			element.classList.remove('optionAreaSelected')
+		} else {
+			$(".optionAreaSelected")[0].classList.remove("optionAreaSelected");
+			element.classList.add('optionAreaSelected');
+		}
+	} else {
+		element.classList.add('optionAreaSelected');
+	}
+});
+
 
 //Select type of order  (imagens)
 $(document).on('click', '.typeArea', function () {
@@ -162,7 +194,7 @@ $(document).on('click', '.categoryArea', function () {
 	}
 });
 
-
+let OPCAO = "";
 let pedido = {}
 let TIPO = "";
 let PRECO = "";
@@ -207,6 +239,18 @@ function loadDuration() {
 		durationArea[2].value = "duracao_l_>20";
 		durationAreaName[2].innerHTML = "A partir de 21 minutos";
 	}
+}
+
+function getOPCAO() {
+	const type = document.getElementsByClassName("optionAreaSelected");
+	if (type[0] == undefined) {
+		document.getElementById("optionError").hidden = false;
+		setTimeout(function () {
+			document.getElementById("optionError").hidden = true;
+		}, 2000);
+		return
+	}
+	return type[0].id;
 }
 
 function getTIPO() {
@@ -280,7 +324,8 @@ $("#orderDrink").click(function () {
 
 	var pedidoFinal = {};
 	loadJSON(pedidoFinal);
-	submeterPedido(pedidoFinal);
+	if (OPCAO == 'conhecimento_manual') submeterPedidoManual(pedidoFinal);
+	if (OPCAO == 'conhecimento_automatico') submeterPedidoAuto(pedidoFinal);
 })
 
 //back
@@ -290,23 +335,13 @@ $("#backBtn").click(function (evt) {
 })
 
 
-async function submeterPedido(pedido) {
+//Conhecimento Manual
+async function submeterPedidoManual(pedido) {
 	let conteudo = "";
 	console.log(pedido);
 
-	const data = {
-		"tipo": "entregar",
-		"preco": "preco_0_7",
-		"duracao": "duracao_e_0_20",
-		"classificacao": "'classificacao_46_47'",
-		"categoria": "carnes",
-		"extras": "extras_nao_incluidos",
-		"bebida": "bebida_nao_incluida"
-	}
-	console.log(data)
-
 	document.getElementById("questionsSection").style.display = "none";
-	document.getElementById("processinfSection").style.display = 'flex';
+	document.getElementById("processingSection").style.display = 'flex';
 
 	//Fetch Headers
 	const myHeaders = new Headers();
@@ -323,7 +358,63 @@ async function submeterPedido(pedido) {
 		mode: 'cors',
 		method: 'POST',
 		headers: myHeaders,
-		body: JSON.stringify(data),
+		body: JSON.stringify(pedido),
+		credentials: 'include'
+	};
+
+	//Fetch
+	fetch('http://127.0.0.1:8080/api/pedido/query?tipo=entregar&preco=preco_0_7&duracao=duracao_e_0_20&classificacao=classificacao_46_47&categoria=carnes&extras=extras_nao_incluidos&bebida=bebida_nao_incluida', requestOptions)
+	//fetch("http://127.0.0.1:8080/api/pedido/query?tipo=" + pedido.tipo + "&preco=" + pedido.preco + "&duracao=" + pedido.duracao + "&classificacao=" + pedido.classificacao + "&categoria=" + pedido.categoria + "&extras=" + pedido.extras + "&bebida=" + pedido.bebida, requestOptions)
+		//fetch('http://127.0.0.1:8080/pedido', requestOptions1)
+		.then(response => response.text())
+		.then(result => {
+			console.log(JSON.parse(result));
+
+			for (const recomendacao of JSON.parse(result)) {
+				conteudo += "<tr><td><img class='imgResult' src='" + recomendacao.imagem + "'></td>";
+				conteudo += "<td>" + recomendacao.nome + "</td>";
+				conteudo += "<td>" + recomendacao.categoria + "</td>";
+				conteudo += "<td>" + recomendacao.restaurante + "</td>";
+				conteudo += "<td>" + recomendacao.duracaoMin + " a " + recomendacao.duracaoMax + "</td>";
+				conteudo += "<td>" + recomendacao.preco + " €</td>";
+				conteudo += "<td>" + recomendacao.localizacao + "</td></tr>";
+			}
+			document.getElementById("showResult").innerHTML = conteudo;
+			document.getElementById("processingSection").style.display = 'none';
+			document.getElementById("answersSection").style.display = "flex";
+
+		}).catch(error => {
+			document.getElementById("processingSection").style.display = 'none';
+			document.getElementById("questionsSection").style.display = "block";
+			console.log('error', error)
+		});
+}
+
+//Conhecimento Automático
+async function submeterPedidoAuto(pedido) {
+	console.log("ola")
+	/*let conteudo = "";
+	console.log(pedido);
+
+	document.getElementById("questionsSection").style.display = "none";
+	document.getElementById("processingSection").style.display = 'flex';
+
+	//Fetch Headers
+	const myHeaders = new Headers();
+	myHeaders.append("Content-Type", "application/json");
+
+	//Fetch requestOptions
+	const requestOptions = {
+		mode: 'cors',
+		method: 'GET',
+		redirect: 'follow'
+	};
+
+	const requestOptions1 = {
+		mode: 'cors',
+		method: 'POST',
+		headers: myHeaders,
+		body: JSON.stringify(pedido),
 		credentials: 'include'
 	};
 
@@ -338,19 +429,19 @@ async function submeterPedido(pedido) {
 			for (const recomendacao of JSON.parse(result)) {
 				conteudo += "<tr><td><img class='imgResult' src='" + recomendacao.imagem + "'></td>";
 				conteudo += "<td>" + recomendacao.nome + "</td>";
-				conteudo += "<td>" + recomendacao.nome + "</td>";
 				conteudo += "<td>" + recomendacao.categoria + "</td>";
 				conteudo += "<td>" + recomendacao.restaurante + "</td>";
 				conteudo += "<td>" + recomendacao.duracaoMin + " a " + recomendacao.duracaoMax + "</td>";
+				conteudo += "<td>" + recomendacao.preco + " €</td>";
 				conteudo += "<td>" + recomendacao.localizacao + "</td></tr>";
 			}
 			document.getElementById("showResult").innerHTML = conteudo;
-			document.getElementById("processinfSection").style.display = 'none';
+			document.getElementById("processingSection").style.display = 'none';
 			document.getElementById("answersSection").style.display = "flex";
 
 		}).catch(error => {
-			document.getElementById("processinfSection").style.display = 'none';
-			document.getElementById("answersSection").style.display = "flex";
+			document.getElementById("processingSection").style.display = 'none';
+			document.getElementById("questionsSection").style.display = "block";
 			console.log('error', error)
-		});
+		});*/
 }
