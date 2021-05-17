2d021679cb52ccb4a3780a00ef2bb4127e5da4f6
JoaoTeixeira89218 <joao07um@gmail.com>
2021-05-17 16:40:36 +0100

    A1 e A2

--- assets/multistepform/js/msform.js
+++ assets/multistepform/js/msform.js
@@ -4,9 +4,10 @@ var current_fs, next_fs, previous_fs; //fieldsets
 var left, opacity, scale; //fieldset properties which we will animate
 var animating; //flag to prevent quick multi-click glitches
 
-
+//Next btn for A1
 $(".next").click(function () {
 	var btn = $(this)[0].id;
+	console.log("ola1")
 
 	if (btn == "orderType") TIPO = getTIPO();
 	if (TIPO == null) return;
@@ -64,7 +65,7 @@ $(".next").click(function () {
 	});
 });
 
-
+//Previous btn for A1
 $(".previous").click(function () {
 	if (animating) return false;
 	animating = true;
@@ -100,19 +101,130 @@ $(".previous").click(function () {
 	});
 });
 
-//******************************************************************************************************************************************** */
 
+//Next btn for A2
+$(".next2").click(function () {
+	var btn = $(this)[0].id;
+	console.log("ola2")
+
+	if (btn == "orderCategory2") CATEGORIA2 = getCATEGORIA2();
+	if (CATEGORIA2 == undefined) return;
+
+	if (btn == "orderGender") GENERO = getGENERO();
+	if (GENERO == undefined) return;
+
+	if (btn == "orderAge2") IDADE2 = getIDADE2();
+	if (IDADE2 == null) return;
+
+	if (btn == "orderDiet") DIETA = getDIETA();
+	if (DIETA == null) return;
+
+	if (btn == "orderPrice2") PRECO2 = getPRECO2();
+	if (PRECO2 == undefined) return;
+
+	if (btn == "orderClassification2") CLASSIFICACAO2 = getCLASSIFICACAO2();
+	if (CLASSIFICACAO2 == undefined) return;
+
+
+	if (animating) return false;
+	animating = true;
+
+	current_fs = $(this).parent();
+	next_fs = $(this).parent().next();
+
+	//activate next step on progressbar using the index of next_fs
+	$("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
+
+	//show the next fieldset
+	next_fs.show();
+	//hide the current fieldset with style
+	current_fs.animate({ opacity: 0 }, {
+		step: function (now, mx) {
+			//as the opacity of current_fs reduces to 0 - stored in "now"
+			//1. scale current_fs down to 80%
+			scale = 1 - (1 - now) * 0.2;
+			//2. bring next_fs from the right(50%)
+			left = (now * 50) + "%";
+			//3. increase opacity of next_fs to 1 as it moves in
+			opacity = 1 - now;
+			current_fs.css({
+				'transform': 'scale(' + scale + ')',
+				'position': 'absolute'
+			});
+			next_fs.css({ 'left': left, 'opacity': opacity });
+		},
+		duration: 800,
+		complete: function () {
+			current_fs.hide();
+			animating = false;
+		},
+		//this comes from the custom easing plugin
+		easing: 'easeInOutBack'
+	});
+});
+
+
+//Previous btn for A2
+$(".previous2").click(function () {
+	if (animating) return false;
+	animating = true;
+
+	current_fs = $(this).parent();
+	previous_fs = $(this).parent().prev();
+
+	//de-activate current step on progressbar
+	$("#progressbar li").eq($("fieldset").index(current_fs)).removeClass("active");
+
+	//show the previous fieldset
+	previous_fs.show();
+	//hide the current fieldset with style
+	current_fs.animate({ opacity: 0 }, {
+		step: function (now, mx) {
+			//as the opacity of current_fs reduces to 0 - stored in "now"
+			//1. scale previous_fs from 80% to 100%
+			scale = 0.8 + (1 - now) * 0.2;
+			//2. take current_fs to the right(50%) - from 0%
+			left = ((1 - now) * 50) + "%";
+			//3. increase opacity of previous_fs to 1 as it moves in
+			opacity = 1 - now;
+			current_fs.css({ 'left': left });
+			previous_fs.css({ 'transform': 'scale(' + scale + ')', 'opacity': opacity });
+		},
+		duration: 800,
+		complete: function () {
+			current_fs.hide();
+			animating = false;
+		},
+		//this comes from the custom easing plugin
+		easing: 'easeInOutBack'
+	});
+});
+
+//******************************************************************************************************************************************** */
+//Select between A1 and A2
 const recomendationOption = document.getElementById("recomendationOption");
 recomendationOption.addEventListener("click", () => {
 	OPCAO = getOPCAO();
 	if (OPCAO == null) return;
-	document.getElementById("optionSection").style.display = 'none';
-	document.getElementById("questionsSection_A1").style.display = 'block';
+	if (OPCAO == 'conhecimento_manual') {
+		document.getElementById("optionSection").style.display = 'none';
+		document.getElementById("questionsSection_A1").style.display = 'block';
+	} else {
+		document.getElementById("optionSection").style.display = 'none';
+		document.getElementById("questionsSection_A2").style.display = 'block';
+	}
 })
 
+//Back to select between A1 and A2 - A1
 const recomendationOptionPrevious = document.getElementById("recomendationOptionPrevious");
 recomendationOptionPrevious.addEventListener("click", () => {
 	document.getElementById("questionsSection_A1").style.display = 'none';
+	document.getElementById("optionSection").style.display = 'flex';
+})
+//Back to select between A1 and A2 - A2
+const recomendationOptionPrevious2 = document.getElementById("recomendationOptionPrevious2");
+recomendationOptionPrevious2.addEventListener("click", () => {
+	document.getElementById("questionsSection_A2").style.display = 'none';
 	document.getElementById("optionSection").style.display = 'flex';
 })
 
@@ -164,7 +276,7 @@ $(document).on('click', '.drinkArea', function () {
 });
 
 
-//Select category of order  (imagens)
+//Select category of order A1 (imagens)
 $(document).on('click', '.categoryArea', function () {
 	var element = $(this)[0].children[0];
 	if ($(".categoryAreaSelected")[0]) {
@@ -179,16 +291,69 @@ $(document).on('click', '.categoryArea', function () {
 	}
 });
 
+
+//Select category of order A2 (imagens)
+$(document).on('click', '.categoryArea2', function () {
+	var element = $(this)[0].children[0];
+	if ($(".categoryAreaSelected2")[0]) {
+		if (element.classList.contains('categoryAreaSelected2')) {
+			element.classList.remove('categoryAreaSelected2')
+		} else {
+			$(".categoryAreaSelected2")[0].classList.remove("categoryAreaSelected2");
+			element.classList.add('categoryAreaSelected2');
+		}
+	} else {
+		element.classList.add('categoryAreaSelected2');
+	}
+});
+
+
+//Select gender of client (imagens)
+$(document).on('click', '.genderArea', function () {
+	var element = $(this)[0].children[0];
+	if ($(".genderAreaSelected")[0]) {
+		if (element.classList.contains('genderAreaSelected')) {
+			element.classList.remove('genderAreaSelected')
+		} else {
+			$(".genderAreaSelected")[0].classList.remove("genderAreaSelected");
+			element.classList.add('genderAreaSelected');
+		}
+	} else {
+		element.classList.add('genderAreaSelected');
+	}
+});
+
+//Variables
 let OPCAO = "";
 let pedido = {}
+let pedido2 = {}
 let TIPO = "";
 let PRECO = "";
+let PRECO2 = "";
 let IDADE = "";
+let IDADE2 = "";
 let CLASSIFICACAO = "";
+let CLASSIFICACAO2 = "";
 let CATEGORIA = "";
+let CATEGORIA2 = "";
 let ESPIRITO = "";
 let BEBIDA = "";
+let GENERO = "";
+let DIETA = "";
+let DURACAO = "";
 
+//Fetch Headers
+const myHeaders = new Headers();
+myHeaders.append("Content-Type", "application/json");
+
+//Fetch requestOptions
+const requestOptions = {
+	mode: 'cors',
+	method: 'GET',
+	redirect: 'follow'
+};
+
+//Load JSON A1
 function loadJSON_A1() {
 	pedido.tipo = TIPO;
 	pedido.preco = PRECO;
@@ -199,42 +364,19 @@ function loadJSON_A1() {
 	pedido.bebida = BEBIDA;
 }
 
+//Load JSON A2
 function loadJSON_A2() {
-	pedido.tipo = TIPO;
-	pedido.preco = PRECO;
-	pedido.idade = IDADE;
-	pedido.classificacao = CLASSIFICACAO;
-	pedido.categoria = CATEGORIA;
-	pedido.espirito = ESPIRITO;
-	pedido.bebida = BEBIDA;
+	pedido2.categorias = CATEGORIA2;
+	pedido2.sexo = GENERO;
+	pedido2.idade = IDADE2;
+	pedido2.dieta = DIETA;
+	pedido2.preco = PRECO2;
+	pedido2.classificacao = CLASSIFICACAO2;
+	pedido2.duracao = DURACAO;
 }
 
-function loadDuration() {
-	const durationArea = $("#durationArea .container2 input")
-	const durationAreaName = $("#durationArea .container2 div")
 
-	if (TIPO == "entregar") {
-		durationArea[0].value = "duracao_e_0_20";
-		durationAreaName[0].innerHTML = "Até 20 minutos";
-
-		durationArea[1].value = "duracao_e_21_29";
-		durationAreaName[1].innerHTML = "Entre 21 a 29 minutos";
-
-		durationArea[2].value = "duracao_e_>=30";
-		durationAreaName[2].innerHTML = "A partir de 30 minutos";
-
-	} else if (TIPO == "levantar") {
-		durationArea[0].value = "duracao_l_0_10";
-		durationAreaName[0].innerHTML = "Até 10 minutos";
-
-		durationArea[1].value = "duracao_l_11_20";
-		durationAreaName[1].innerHTML = "Entre 11 a 20 minutos";
-
-		durationArea[2].value = "duracao_l_>20";
-		durationAreaName[2].innerHTML = "A partir de 21 minutos";
-	}
-}
-
+//Get option
 function getOPCAO() {
 	const type = document.getElementsByClassName("optionAreaSelected");
 	if (type[0] == undefined) {
@@ -247,6 +389,7 @@ function getOPCAO() {
 	return type[0].id;
 }
 
+//Get type of order
 function getTIPO() {
 	const type = document.getElementsByClassName("typeAreaSelected");
 	if (type[0] == undefined) {
@@ -268,6 +411,15 @@ function getIDADE() {
 		document.getElementById("ageError").hidden = true;
 	}, 2000);
 }
+//Get order price
+function getIDADE2() {
+	var checkRadio = document.querySelector('input[name="age2"]:checked');
+	if (checkRadio != null) return checkRadio.value;
+	document.getElementById("ageError2").hidden = false;
+	setTimeout(function () {
+		document.getElementById("ageError2").hidden = true;
+	}, 2000);
+}
 
 //Get order price
 function getPRECO() {
@@ -276,6 +428,15 @@ function getPRECO() {
 	document.getElementById("priceError").hidden = false;
 	setTimeout(function () {
 		document.getElementById("priceError").hidden = true;
+	}, 2000);
+}
+//Get order price
+function getPRECO2() {
+	var checkRadio = document.querySelector('input[name="price2"]:checked');
+	if (checkRadio != null) return checkRadio.value;
+	document.getElementById("priceError2").hidden = false;
+	setTimeout(function () {
+		document.getElementById("priceError2").hidden = true;
 	}, 2000);
 }
 
@@ -298,7 +459,17 @@ function getCLASSIFICACAO() {
 		document.getElementById("classificationError").hidden = true;
 	}, 2000);
 }
+//Get classification
+function getCLASSIFICACAO2() {
+	var checkRadio = document.querySelector('input[name="classification2"]:checked');
+	if (checkRadio != null) return checkRadio.value;
+	document.getElementById("classificationError2").hidden = false;
+	setTimeout(function () {
+		document.getElementById("classificationError2").hidden = true;
+	}, 2000);
+}
 
+//Get Category
 function getCATEGORIA() {
 	const type = document.getElementsByClassName("categoryAreaSelected");
 	if (type[0] == undefined) {
@@ -310,8 +481,20 @@ function getCATEGORIA() {
 	}
 	return type[0].id;
 }
+//Get Category
+function getCATEGORIA2() {
+	const type = document.getElementsByClassName("categoryAreaSelected2");
+	if (type[0] == undefined) {
+		document.getElementById("categoryError2").hidden = false;
+		setTimeout(function () {
+			document.getElementById("categoryError2").hidden = true;
+		}, 2000);
+		return
+	}
+	return type[0].id;
+}
 
-
+//Get dink
 function getBEBIDA() {
 	const type = document.getElementsByClassName("drinkAreaSelected");
 	if (type[0] == undefined) {
@@ -324,34 +507,91 @@ function getBEBIDA() {
 	return type[0].id;
 }
 
-//Submit
+//Get Gender
+function getGENERO() {
+	const type = document.getElementsByClassName("genderAreaSelected");
+	if (type[0] == undefined) {
+		document.getElementById("genderError").hidden = false;
+		setTimeout(function () {
+			document.getElementById("genderError").hidden = true;
+		}, 2000);
+		return
+	}
+	return type[0].id;
+}
+
+//Get client diet
+function getDIETA() {
+	console.log("p")
+	var checkRadio = document.querySelector('input[name="diet"]:checked');
+	if (checkRadio != null) return checkRadio.value;
+	document.getElementById("dietError").hidden = false;
+	setTimeout(function () {
+		document.getElementById("dietError").hidden = true;
+	}, 2000);
+}
+
+//Get order duration
+function getDURACAO() {
+	var checkRadio = document.querySelector('input[name="duration"]:checked');
+	if (checkRadio != null) return checkRadio.value;
+	document.getElementById("durationError").hidden = false;
+	setTimeout(function () {
+		document.getElementById("durationError").hidden = true;
+	}, 2000);
+}
+
+
+//Submit A1
 $("#orderDrink").click(function () {
 	var btn = $(this)[0].id;
 	if (btn == "orderDrink") BEBIDA = getBEBIDA();
 	if (BEBIDA == undefined) return
 
-	if (OPCAO == 'conhecimento_manual')	loadJSON_A1(); submeterPedidoManual();
-	if (OPCAO == 'conhecimento_automatico')loadJSON_A2(); submeterPedidoAuto();
+	if (OPCAO == 'conhecimento_manual') {
+		loadJSON_A1();
+		submeterPedidoManual();
+	}
+	if (OPCAO == 'conhecimento_automatico') {
+		loadJSON_A2();
+		submeterPedidoAuto();
+	}
+})
+
+//Submit A2 
+$("#orderDuration").click(function () {
+	var btn = $(this)[0].id;
+	if (btn == "orderDuration") DURACAO = getDURACAO();
+	if (DURACAO == undefined) return
+
+	if (OPCAO == 'conhecimento_manual') {
+		loadJSON_A1();
+		submeterPedidoManual();
+	}
+	if (OPCAO == 'conhecimento_automatico') {
+		loadJSON_A2();
+		submeterPedidoAuto();
+	}
 })
 
 
-//back
-$("#backBtn").click(function (evt) {
+//Back to star
+$(".backBtn").click(function (evt) {
 	evt.preventDefault();
 	location.reload();
 })
 
-//Fetch Headers
-const myHeaders = new Headers();
-myHeaders.append("Content-Type", "application/json");
+//Back to questionnaire
+$("#backBtn2").click(function (evt) {
+	evt.preventDefault();
+	document.getElementById("NoAnswersSection").style.display = 'none';
+	if (OPCAO == 'conhecimento_manual') {
+		document.getElementById("questionsSection_A1").style.display = "block";
+	} else {
+		document.getElementById("questionsSection_A2").style.display = "block";
+	}
 
-//Fetch requestOptions
-const requestOptions = {
-	mode: 'cors',
-	method: 'GET',
-	redirect: 'follow'
-};
-
+})
 
 //Conhecimento Manual
 async function submeterPedidoManual() {
@@ -363,7 +603,7 @@ async function submeterPedidoManual() {
 
 	//Fetch
 	//fetch('http://127.0.0.1:8080/api/tarefa-a1/query?tipo=entregar&preco=preco_0_7&idade=idade_1829&estado_espirito=apressado&classificacao=classificacao_46_47&categoria=carnes&bebida=bebida_nao_incluida', requestOptions)
-	fetch("http://127.0.0.1:8080/api/tarefa-a1/query?tipo=" + pedido.tipo + "&preco=" + pedido.preco + "&idade=" + pedido.idade + "&espirito=" + pedido.espirito +"&classificacao=" + pedido.classificacao + "&categoria=" + pedido.categoria + "&bebida=" + pedido.bebida, requestOptions)
+	fetch("http://127.0.0.1:8080/api/tarefa-a1/query?tipo=" + pedido.tipo + "&preco=" + pedido.preco + "&idade=" + pedido.idade + "&espirito=" + pedido.espirito + "&classificacao=" + pedido.classificacao + "&categoria=" + pedido.categoria + "&bebida=" + pedido.bebida, requestOptions)
 		.then(response => response.text())
 		.then(result => {
 			console.log(JSON.parse(result));
@@ -383,13 +623,37 @@ async function submeterPedidoManual() {
 
 		}).catch(error => {
 			document.getElementById("processingSection").style.display = 'none';
-			document.getElementById("questionsSection_A1").style.display = "block";
+			document.getElementById("NoAnswersSection").style.display = "flex";
 			console.log('error', error)
 		});
 }
 
 //Conhecimento Automático
 async function submeterPedidoAuto() {
-	console.log("ola")
+	let conteudo = "";
+	console.log(pedido2);
 
+	document.getElementById("questionsSection_A2").style.display = "none";
+	document.getElementById("processingSection").style.display = 'flex';
+
+	//Fetch
+	//fetch('http://127.0.0.1:8080/api/tarefa-a1/query?tipo=entregar&preco=preco_0_7&idade=idade_1829&estado_espirito=apressado&classificacao=classificacao_46_47&categoria=carnes&bebida=bebida_nao_incluida', requestOptions)
+	fetch("http://127.0.0.1:8080/api/tarefa-a1/query?tipo=" + pedido.tipo + "&preco=" + pedido.preco + "&idade=" + pedido.idade + "&espirito=" + pedido.espirito + "&classificacao=" + pedido.classificacao + "&categoria=" + pedido.categoria + "&bebida=" + pedido.bebida, requestOptions)
+		.then(response => response.text())
+		.then(result => {
+			console.log(JSON.parse(result));
+
+
+
+
+
+			document.getElementById("showResult").innerHTML = conteudo;
+			document.getElementById("processingSection").style.display = 'none';
+			document.getElementById("answersSection_A2").style.display = "flex";
+
+		}).catch(error => {
+			document.getElementById("processingSection").style.display = 'none';
+			document.getElementById("NoAnswersSection").style.display = "flex";
+			console.log('error', error)
+		});
 }
